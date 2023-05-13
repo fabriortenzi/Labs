175b7ff7dba58f823f32e9d1c2976ca973ca1aa7
fabriortenzi <fabriortenzi10@gmail.com>
2023-05-13 15:06:14 -0300

    Ejercicio 1 terminado

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/LabClases1/Clases/A.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Clases
+{
+    public class A
+    {
+        private string NombreInstancia;
+
+        public A()
+        {
+            NombreInstancia = "Instancia sin Nombre";
+        }
+
+        public A(string nombre)
+        {
+            NombreInstancia = nombre;
+        }
+
+        public void MostrarNombre()
+        {
+            Console.WriteLine($"Nombre de la Instancia: {NombreInstancia}");
+        }
+
+        public void M1()
+        {
+            Console.WriteLine("El metodo M1 fue invocado");
+        }
+
+        public void M2()
+        {
+            Console.WriteLine("El metodo M2 fue invocado");
+        }
+
+        public void M3()
+        {
+            Console.WriteLine("El metodo M3 fue invocado");
+        }
+    }
+}
