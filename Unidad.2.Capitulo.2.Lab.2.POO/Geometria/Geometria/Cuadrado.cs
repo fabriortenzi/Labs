636ccb21657d8fcb8fc69ad0996377f75a9c8e79
fabriortenzi <fabriortenzi10@gmail.com>
2023-05-19 17:25:48 -0300

    Ejercicio 3 terminado

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/Geometria/Geometria/Cuadrado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace Geometria
+{
+    public class Cuadrado : Rectangulo
+    {
+        public override float CalcularPerimetro()
+        {
+            return (Base * 4);
+        }
+
+        public override float CalcularSuperficie()
+        {
+            return (float) Math.Pow(Base, 2);
+        }
+    }
+}
---
+++ Unidad.2.Capitulo.2.Lab.2.POO/Geometria/Geometria/Poligono.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometria
+{
+    abstract public class Poligono
+    {
+        abstract public float CalcularPerimetro();
+
+        abstract public float CalcularSuperficie();
+    }
+}
