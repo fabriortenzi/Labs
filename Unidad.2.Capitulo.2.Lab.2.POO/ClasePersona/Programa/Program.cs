3604d878b71d6f8ddb2ce4c121bad5bd3a437856
fabriortenzi <fabriortenzi10@gmail.com>
2023-05-23 08:57:01 -0300

    Ejercicio 4 terminado

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/ClasePersona/Programa/Program.cs
@@ -0,0 +1,7 @@
+﻿using ClasePersona;
+
+Persona persona1 = new("Fabrizio", "Ortenzi", 22, 91091010);
+
+Console.WriteLine(persona1.GetFullName());
+
+Console.WriteLine(persona1.GetAge(new DateOnly(2001, 1, 10)));
