b30ebf90f40a280cf57076fa18ce3f90a2eb6452
fabriortenzi <fabriortenzi10@gmail.com>
2023-06-03 11:31:26 -0300

    Laboratorio LINQ terminado

--- Unidad.2.Extra.Lab.3.LINQ/FuncionesLINQ/FuncionesLinq.cs
+++ Unidad.2.Extra.Lab.3.LINQ/FuncionesLINQ/FuncionesLinq.cs
@@ -8,22 +8,32 @@ namespace FuncionesLINQ
     {
         public IEnumerable<string> ObtenerProvinciasQueEmpiezanConDeterminadasLetras(IEnumerable<string> provincias)
         {
-            throw new NotImplementedException();
+            var provinciasQueEmpiezanConSoT = provincias.Where(x => x.StartsWith("S") || x.StartsWith("T"));
+            return provinciasQueEmpiezanConSoT;
         }
 
         public IEnumerable<int> ObtenerNumerosMayoresA20(IEnumerable<int> numeros)
         {
-            throw new NotImplementedException();
+            var numerosMayoresA20 = numeros.Where(x => x > 20);
+            return numerosMayoresA20;
         }
 
         public IEnumerable<int> ObtenerCodigoPostalDeCiudadesQueTenganEnSuNombreTresCarateresDeterminados(IEnumerable<Ciudad> ciudades, string ciudad)
         {
-            throw new NotImplementedException();
+            var codigosPostales = ciudades.Where(x => x.Nombre.ToUpper().Contains(ciudad.ToUpper())).Select(x => x.CodigoPostal);
+            return codigosPostales;
         }
 
         public IEnumerable<Empleado> AgregarEmpleadoListaDevolviendolaOrdenadaPorSueldo(IEnumerable<Empleado> empleados, IEnumerable<Empleado> empleadosParaAgregar, string order)
         {
-            throw new NotImplementedException();
+            var empleadosTotales = empleados.Concat(empleadosParaAgregar);
+
+            if (order.ToUpper() == "ASC")
+                empleadosTotales = empleadosTotales.OrderBy(x => x.Sueldo);
+            else if (order.ToUpper() == "DESC")
+                empleadosTotales = empleadosTotales.OrderByDescending(x => x.Sueldo);
+
+            return empleadosTotales;
         }
     }
 }
