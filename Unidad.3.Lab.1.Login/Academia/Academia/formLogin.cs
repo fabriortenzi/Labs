0b57cdac6db98198d192117f71844fa1671d1729
fabriortenzi <fabriortenzi10@gmail.com>
2023-06-08 19:32:27 -0300

    Init Unidad 3 Lab1
    Ejercicio 2 terminado

---
+++ Unidad.3.Lab.1.Login/Academia/Academia/formLogin.cs
@@ -0,0 +1,28 @@
+namespace Academia
+{
+    public partial class formLogin : Form
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            //la propiedad Text de los TextBox contiene el texto escrito en ellos
+            if (this.txtUsuario.Text == "Admin" && this.txtPass.Text == "admin")
+            {
+                MessageBox.Show("Usted ha ingresado al sistema correctamente.", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña incorrectos", "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lnkOlvidaPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Es Ud. un usuario muy descuidado, haga memoria", "Olvidé mi contraseña", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
